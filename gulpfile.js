301d575d908948d7ecdc8d25f240d7234d02146f
discopatrick <git@discopatrick.me>
2017-07-16 20:07:47 +0100

    Make gulpfile non-venv-specific for easier use by other developers

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,16 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 
 gulp.task('test', shell.task([
-  'venv3.6/bin/python manage.py test'
+  'python manage.py test'
 ]));
 
 gulp.task('flake8', shell.task([
-  'venv3.6/bin/flake8 monitoring/ servers/ --exclude migrations'
+  'flake8 monitoring/ servers/ --exclude migrations'
 ]));
 
 gulp.task('autopep8', shell.task([
-    'venv3.6/bin/autopep8 monitoring/ --in-place --recursive --exclude migrations',
-    'venv3.6/bin/autopep8 servers/ --in-place --recursive'
+    'autopep8 monitoring/ --in-place --recursive --exclude migrations',
+    'autopep8 servers/ --in-place --recursive'
 ]));
 
 gulp.task('watch', function(){
