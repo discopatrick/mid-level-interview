bb357e131094f954debaf302a2f40e2fbe2bef43
discopatrick <git@discopatrick.me>
2017-07-16 17:31:47 +0100

    Add datetime field to LoginRecord

--- monitoring/tests.py
+++ monitoring/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django.utils import timezone
+
 from .models import LoginRecord, ServerUser
 
 
@@ -6,10 +8,27 @@ class LoginRecordTestCase(TestCase):
 
     def test_relate_loginrecord_to_serveruser(self):
         user = ServerUser.objects.create(username='patrick')
-        login_record = LoginRecord.objects.create(server_user=user)
+        login_record = LoginRecord.objects.create(
+            server_user=user,
+            datetime=timezone.now()
+        )
         retrieved_login_record = LoginRecord.objects.get(pk=login_record.pk)
 
         self.assertEqual(retrieved_login_record.server_user, user)
+
+    def test_add_datetime_to_loginrecord(self):
+
+        user = ServerUser.objects.create(username='patrick')
+        login_record = LoginRecord.objects.create(
+            server_user=user,
+            datetime=timezone.now()
+        )
+        retrieved_login_record = LoginRecord.objects.get(pk=login_record.pk)
+
+        self.assertEqual(
+            retrieved_login_record.datetime,
+            login_record.datetime
+        )
 
 
 class ServerUserTestCase(TestCase):
