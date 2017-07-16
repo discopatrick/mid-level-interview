33a528732c2a5a0933a803a07e8e98a60e1c862d
discopatrick <git@discopatrick.me>
2017-07-16 17:10:24 +0100

    Update import command to create ServerUsers (or use existing ones)

--- monitoring/management/commands/import.py
+++ monitoring/management/commands/import.py
@@ -2,7 +2,7 @@ import csv
 
 from django.core.management import BaseCommand
 
-from monitoring.models import LoginRecord
+from monitoring.models import LoginRecord, ServerUser
 
 
 class Command(BaseCommand):
@@ -14,12 +14,20 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         login_records_created = 0
+        server_users_created = 0
 
         with open(options['file']) as f:
             reader = csv.reader(f)
             for row in reader:
-                LoginRecord.objects.create(user=row[self.USERNAME_INDEX])
+                username = row[self.USERNAME_INDEX]
+                server_user, created = ServerUser.objects.get_or_create(
+                    username=username)
+                if created:
+                    server_users_created += 1
+                LoginRecord.objects.create(server_user=server_user)
                 login_records_created += 1
 
+        self.stdout.write("{} new server users created.".format(
+                          server_users_created))
         self.stdout.write("{} login records created.".format(
                           login_records_created))
