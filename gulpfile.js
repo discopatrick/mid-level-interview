3eaa41f5b11a101162a9e19073868d33276e89a6
discopatrick <git@discopatrick.me>
2017-07-16 16:12:44 +0100

    Add flake8 to requirements, and run on gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,17 @@ gulp.task('test', shell.task([
   'venv3.6/bin/python manage.py test'
 ]));
 
+gulp.task('flake8', shell.task([
+  'venv3.6/bin/flake8 monitoring/ servers/'
+]));
+
 gulp.task('watch', function(){
   gulp.watch(
     [
       './servers/**/*.py',
       './monitoring/**/*.py'
     ],
-    ['test']
+    ['flake8', 'test']
   )
 })
 
